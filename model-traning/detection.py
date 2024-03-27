cba8c3499fa8862f5883fcb21bf3ceffc9ec81d0
Rayen ben brahim <129397918+RAYEN311@users.noreply.github.com>
2024-03-27 03:09:45 -0700

    Add files via upload

---
+++ model-traning/detection.py
@@ -0,0 +1,17 @@
+from detecto.core import Model
+from detecto import utils, visualize
+
+labels = ['PCB-MODEL-0', 'normal-spring_finger', 'defected-spring_finger','normal-through','defected-through']
+
+model = Model.load('./pcb_detect.pth' , labels )
+
+
+
+image1 = utils.read_image('./datasets/images/validation/1710111756965.jpg')
+
+images = [image1]
+
+visualize.plot_prediction_grid(model,images, score_filter= 0.3 )
+
+
+# visualize.detect_live(model , 0.8) 
---
+++ model-traning/traning.py
@@ -0,0 +1,19 @@
+from detecto.core import Model, Dataset
+
+dataset = Dataset('./datasets/images/train')
+
+model = Model(['PCB-MODEL-0', 'normal-spring_finger', 'defected-spring_finger','normal-through','defected-through'])
+
+model.fit(dataset)
+
+model.save('pcb_detect.pth')
+
+
+
+
+
+# PCB-MODEL-0
+# normal-spring_finger
+# defected-spring_finger
+# normal-through
+# defected-through
