92366fb89677617db8a449596a25a6cedc0a8a7d
Rayen ben brahim <129397918+RAYEN311@users.noreply.github.com>
2024-03-27 15:13:18 -0700

    Update detection.py

--- model-traning/detection.py
+++ model-traning/detection.py
@@ -12,6 +12,4 @@ image1 = utils.read_image('./datasets/images/validation/1710111756965.jpg')
 images = [image1]
 
 visualize.plot_prediction_grid(model,images, score_filter= 0.3 )
-
-
-# visualize.detect_live(model , 0.8) 
+visualize.detect_live(model , 0.8) 
