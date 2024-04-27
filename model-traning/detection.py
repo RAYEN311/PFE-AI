d7000e93d23cb5feeeec5e5251bed5dccfdfc855
Rayen ben brahim <129397918+RAYEN311@users.noreply.github.com>
2024-04-27 12:35:01 -0700

    Update detection.py

--- model-traning/detection.py
+++ model-traning/detection.py
@@ -5,7 +5,7 @@ labels = ['PCB-MODEL-0', 'normal-spring_finger', 'defected-spring_finger','norma
 
 model = Model.load('./pcb_detect.pth' , labels )
 
-
+#
 
 image1 = utils.read_image('./datasets/images/validation/1710111756965.jpg')
 
